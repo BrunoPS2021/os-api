1db3abdf6f3d43b0a2c225b3c1e32f7645cc0229
Silva <bruno.pereira.silva@accenture.com>
2024-05-13 17:25:31 -0300

    ajustes na API

--- src/main/java/com/bruno/pereira/silva/os/dtos/OSDTO.java
+++ src/main/java/com/bruno/pereira/silva/os/dtos/OSDTO.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 import com.bruno.pereira.silva.os.domain.Cliente;
 import com.bruno.pereira.silva.os.domain.OS;
 import com.bruno.pereira.silva.os.domain.Tecnico;
+import com.bruno.pereira.silva.os.domain.enuns.Prioridade;
+import com.bruno.pereira.silva.os.domain.enuns.Status;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.NotEmpty;
@@ -68,8 +70,8 @@ public class OSDTO implements Serializable{
 		this.dataFechamento = dataFechamento;
 	}
 
-	public Integer getPrioridade() {
-		return prioridade;
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
 	}
 
 	public void setPrioridade(Integer prioridade) {
@@ -84,8 +86,8 @@ public class OSDTO implements Serializable{
 		this.observacoes = observacoes;
 	}
 
-	public Integer getStatus() {
-		return status;
+	public Status getStatus() {
+		return Status.toEnum(this.status);
 	}
 
 	public void setStatus(Integer status) {
